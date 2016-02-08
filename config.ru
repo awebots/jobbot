bf12c2f6276871ca30d4eada15f7f85c6e48daef
HugoDF <hdifrancesco@gmail.com>
2016-02-08 14:25:30 +0000

    lots of work, refactor, make the bot ready for deploy

---
+++ config.ru
@@ -0,0 +1,1 @@
+ruby ./jobbot.rb
---
+++ job.rb
@@ -0,0 +1,38 @@
+class Job
+  def initialize (origin, id, title, description, created_at, link)
+    @origin = origin
+    @id = id
+    @title = title
+    @description = description
+    @created_at = created_at
+    @link = link
+  end
+  def origin
+    @origin
+  end
+  def id
+    @id
+  end 
+  def description
+    @description
+  end
+  def created_at
+    @created_at
+  end
+  def format format
+    formatted =''
+    if format.include?"id"
+      formatted += "id: " + @id + "\n"
+    end
+    if format.include?"link"
+      formatted += "Link: " + @link + "\n"
+    end
+    if format.include?"title"
+      formatted += "Title: " + @title + "\n"
+    end
+    if format.include?"description"
+      formatted += "Text: " + @description
+    end
+    formatted
+  end
+end
