fa41dfb6cffb36b2ebac7e3a93738253890bedd7
HugoDF <hdifrancesco@gmail.com>
2016-02-13 22:01:28 +0000

    update JobGrabber to muti-threaded form

--- lib/job_grabber.rb
+++ lib/job_grabber.rb
@@ -1,13 +1,20 @@
 require_relative 'src_grabber'
+require 'thread'
 class JobGrabber
   def initialize(sources)
     @sources = sources
   end
   def grab
     jobs = Array.new
+    mutex = Mutex.new
+    threads = Array.new
     @sources.each do |src|
-      jobs += SrcGrabber.new(src).grab
+      threads << Thread.new(src, jobs) do |src, jobs|
+        src_jobs = SrcGrabber.new(src).grab
+        mutex.synchronize{jobs += src_jobs}
+      end
     end
+    threads.each(&:join)
     jobs
   end
 end
